ae766bb698d25ec57429077d2607e305d7a0c5e1
Diye Mark <diyye101@gmail.com>
2022-07-16 12:33:56 +0300

    wip: contract

--- refund-by-location/contracts/RefundByLocation.sol
+++ refund-by-location/contracts/RefundByLocation.sol
@@ -1,37 +1,54 @@
 pragma solidity ^0.8.0;
 
+//import "hardhat/console.sol";
+
+
 contract RefundByLocation {
-    enum StateType { Created, Completed, OutOfCompliance }
+//    enum StateType { Created, Completed, OutOfCompliance }
 
     struct Coordinate {
         uint256 lat;
         uint256 long;
     }
 
-    StateType public State;
+//    StateType public State;
     address public Owner;
+    uint256 public Radius;
     Coordinate public OfficeLocation;
-    address public Employee;
     int public LastLocationUpdateTimestamp;
 
-    mapping (address => Coordinate) public EmployeeInfo;
+    struct EmployeeLocationInfo {
+        mapping (uint256 => Coordinate) LocationAtTimeStamp;
+    }
 
-    function() public payable {}
+    mapping (address => EmployeeLocationInfo) EmployeeLocationInfos;
 
-    constructor(address owner, Coordinate officeLocation) public {
+    // function() public payable {}
+
+    constructor(address owner,uint256 officeLocationLat, uint256 officeLocationLong ,uint256 radius) public {
         Owner = owner;
-        OfficeLocation = officeLocation;
+        OfficeLocation = Coordinate({lat: officeLocationLat, long: officeLocationLong });
+        Radius = radius;
+    }
+
+    function getEmployeeLocationInfor(address employeeAdd, uint256 timeStamp) public view {
+
+        EmployeeLocationInfo storage employeeLocation = EmployeeLocationInfos[employeeAdd];
+
+//        console.log("over here lat", employeeLocation.LocationAtTimeStamp[timeStamp].lat);
+
+//        console.log("over here long", employeeLocation.LocationAtTimeStamp[timeStamp].long);
+
+        // return employeeLocation[timeStamp];
     }
 
     // check if the location of the employee is in range
-    function isUserLocationInRange(Coordinate employeeLocation) private {}
+    function isUserLocationInRange(Coordinate memory employeeLocation) private {}
 
     // report the location of the employee
-    function reportLocation(Coordinate employeeLocation) public payable {}
-
-    // kill contract
-    function kill() public {
-        if(msg.sender == owner) selfdestruct(owner);
+    function reportLocation(uint256 employeeLocationCoordinateLat, uint256 employeeLocationCoordinateLong , uint256 timeStamp) public {
+        EmployeeLocationInfo storage employeeLocationInfo = EmployeeLocationInfos[msg.sender];
+        employeeLocationInfo.LocationAtTimeStamp[timeStamp] = Coordinate({lat: employeeLocationCoordinateLat, long: employeeLocationCoordinateLong });
+        // EmployeeLocationInfos[msg.sender] = employeeLocationInfo;
     }
-
 }
